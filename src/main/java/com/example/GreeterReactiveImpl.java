a97d7a62f2cc17a4f36aab4e4a9a4ce31691f611
linux_china <libing.chen@gmail.com>
2021-04-19 20:40:07 -0700

    Polishing

--- src/main/java/com/example/GreeterReactiveImpl.java
+++ src/main/java/com/example/GreeterReactiveImpl.java
@@ -1,6 +1,7 @@
 package com.example;
 
-import demo.CustomerProtos;
+import demo.Customer;
+import demo.CustomerRequest;
 import demo.ReactorGreeterGrpc;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -8,8 +9,8 @@ import reactor.core.publisher.Mono;
 @Service
 public class GreeterReactiveImpl extends ReactorGreeterGrpc.GreeterImplBase {
     @Override
-    public Mono<CustomerProtos.Customer> hello(Mono<CustomerProtos.CustomerRequest> request) {
-        CustomerProtos.Customer reply = CustomerProtos.Customer.newBuilder()
+    public Mono<Customer> hello(Mono<CustomerRequest> request) {
+        Customer reply = Customer.newBuilder()
                 .setId(1).setFirstName("Josh").setLastName("Long")
                 .build();
         return Mono.just(reply);
