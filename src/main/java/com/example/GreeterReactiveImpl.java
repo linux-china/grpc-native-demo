0809b327f6b5edf469e4d6fb3a6787ce1542d938
linux_china <libing.chen@gmail.com>
2021-05-04 13:46:19 -0700

    Enable @GRpcService

--- src/main/java/com/example/GreeterReactiveImpl.java
+++ src/main/java/com/example/GreeterReactiveImpl.java
@@ -3,10 +3,12 @@ package com.example;
 import demo.Customer;
 import demo.CustomerRequest;
 import demo.ReactorGreeterGrpc;
+import org.lognet.springboot.grpc.GRpcService;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
+@GRpcService
 public class GreeterReactiveImpl extends ReactorGreeterGrpc.GreeterImplBase {
     @Override
     public Mono<Customer> hello(Mono<CustomerRequest> request) {
